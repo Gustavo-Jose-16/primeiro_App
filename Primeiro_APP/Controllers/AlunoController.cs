ee4d6b0e512193c71a669d97a7dacd23c9f5bd74
GUSTAVO JOSE ANDRADE DA SILVA <00001118636387sp@aluno.educacao.sp.gov.br>
2026-03-10 17:00:00 -0300

    adicionando conteudo

---
+++ Primeiro_APP/Controllers/AlunoController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Primeiro_APP.Controllers
+{
+    public class AlunoController : Controller
+    {
+        [ViewData] public string nome { get; set; }        
+        [ViewData] public string sobrenome { get; set; }
+        
+        public IActionResult Index()
+        {
+            ViewBag.nota = "Aluno nota 10";
+            ViewData["Escola"] = "Escola Pedro Fonseca";
+
+            nome = "Nilson";
+            sobrenome = "Alcides";      
+
+            return View();
+        }
+
+        public IActionResult Teste()
+        {
+            TempData["MSG_EX"] = "Mensagem do TempData";
+            return RedirectToAction("Resultado");
+        }
+        public IActionResult Resultado() 
+        {
+            if (TempData["MSG_EX"] != null)
+                ViewBag.Exemplo = "Alunos do 3°B";            
+            return View();
+        }
+        public IActionResult Redirecionamemto()
+        {
+            return Redirect("https://www.google.com.br");
+        }
+
+        public IActionResult RedirecionamentoRota()
+        {
+            return RedirectToRoute(new { Controller = "Aluno", action = "Resultado" });
+        }
+
+        public IActionResult RedirecionamentoACT()
+        {
+            return RedirectToAction("Resultado", "Aluno");
+        }
+    }
+}
+
